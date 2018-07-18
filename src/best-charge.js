7af09d8f7a30e93c2e2a4d47ca32dbce6ca600bb
Sen Chen <senchen@CNsenchen.local>
2018-07-18 21:47:17 +0800

    分别打印出在各个优惠类型情况下的优惠情况

--- src/best-charge.js
+++ src/best-charge.js
@@ -10,7 +10,7 @@ function bestCharge(selectedItems) {
     dish_datamap.set(element['id'], element);
   });
 
-  half_dish_list = all_promo['items'];
+  half_dish_list = all_promo[1]['items'];
 
   let dish_list = new Map();
   selectedItems.forEach(element => {
@@ -19,6 +19,10 @@ function bestCharge(selectedItems) {
   });
 
   let all_price = 0;
+  let prom1_price = 0;
+  let prom2_price = 0;
+  let prom2_dishs = new Array();
+
   Array.from(dish_list.keys()).forEach(element => {
     let price = dish_list.get(element)*dish_datamap.get(element)['price'];
     res += '\n' + dish_datamap.get(element)['name'] + ' x ' + dish_list.get(element) + ' = ' +
@@ -27,7 +31,28 @@ function bestCharge(selectedItems) {
      all_price += price;
   });
 
-  //res += '\n-----------------------------------';
+  Array.from(dish_list.keys()).forEach(element => {
+    if(half_dish_list.includes(element)) {
+      prom2_price += dish_list.get(element)*dish_datamap.get(element)['price'] / 2;
+      prom2_dishs.push(dish_datamap.get(element)['name']);
+    }
+  });
+
+  if(all_price >= 30) {
+    prom1_price = 6;
+  }
+
+  if(prom1_price > 0 || prom2_price > 0) {
+    res += '\n-----------------------------------\n使用优惠:\n';
+    if(prom1_price >= prom2_price) {
+      res += '满30减6元，省6元';
+    } else {
+      res += '指定菜品半价(' + prom2_dishs.join('，') + ')，省' +prom2_price + '元';
+    }
+  }
+
+
+
 
   res += '\n-----------------------------------\n总计：'
   res += all_price + '元';
